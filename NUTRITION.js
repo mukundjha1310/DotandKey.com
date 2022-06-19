beb9ae962b57df4116f4c1ba859b252c80b58df8
Suraj Pratap <advocatesurajpratap95@gmail.com>
2022-06-19 12:12:54 +0530

    final changes

--- NUTRITION.js
+++ NUTRITION.js
@@ -1,1 +1,28 @@
 // write code here
+function what_to_show() {
+    let len = JSON.parse(localStorage.getItem("login-data")) || [];
+    if (len.length > 0) {
+      window.location.href = "details.html";
+    } else {
+      window.location.href = "login.html";
+    }
+  }
+
+  function yes(){
+    let x=document.querySelector("#filter").value;
+    if(x=="Whats New"){
+        window.location.href = "newArrivel.html";
+     }else if(x=="hair care"){
+        window.location.href = "HairCare.html";
+     }else if(x=="body care"){
+        window.location.href = "BodyCare.html";
+     }else if(x=="app"){
+        window.location.href ="https://onelink.to/4bzhqy";
+     }else if(x=="knowUsBatter"){
+        window.location.href ="aboutus.html";
+     }else if(x=="profile"){
+        window.location.href ="details.html";
+     }else if(x=="login"){
+        window.location.href ="login.html";
+     }
+  }
--- SERUM.js
+++ SERUM.js
@@ -71,7 +71,39 @@ let serumdata=[
        },
     ]
 
-  // let data=JSON.parse(localStorage.getItem("serumdata"))||[]
+  let data=JSON.parse(localStorage.getItem("serumdata")) || []
+
+
+  function what_to_show() {
+    let len = JSON.parse(localStorage.getItem("login-data")) || [];
+    if (len.length > 0) {
+      window.location.href = "details.html";
+    } else {
+      window.location.href = "login.html";
+    }
+  }
+
+  function yes(){
+    let x=document.querySelector("#filter").value;
+    if(x=="Whats New"){
+        window.location.href = "newArrivel.html";
+     }else if(x=="hair care"){
+        window.location.href = "HairCare.html";
+     }else if(x=="body care"){
+        window.location.href = "BodyCare.html";
+     }else if(x=="app"){
+        window.location.href ="https://onelink.to/4bzhqy";
+     }else if(x=="knowUsBatter"){
+        window.location.href ="aboutus.html";
+     }else if(x=="profile"){
+        window.location.href ="details.html";
+     }else if(x=="login"){
+        window.location.href ="login.html";
+     }
+  }
+
+
+
 
   displayproduct(serumdata)
 
@@ -115,5 +147,6 @@ function  displayproduct(serumdata)
 function serumpage(ele)
     {
    localStorage.setItem("serumdata",JSON.stringify(ele))
-   window.location.href="./serumLS/serumLS.html"
+   window.location.href="./serumLS.html"
     }
+
